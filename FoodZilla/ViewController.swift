296810e5e2b990a5d30caf8c0424a4daacc56fa2
Denis Rakitin <piggest@mail.ru>
2019-09-07 22:02:40 +0200

    Initial Commit

---
+++ FoodZilla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodZilla
+//
+//  Created by Denis Rakitin on 2019-09-07.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
